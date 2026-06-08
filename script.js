d81ce0e0b619f023832ffd68791c851ceb17fb1d
sundarchincholi <sundarchincholi75@gmail.com>
2026-06-08 10:17:05 +0530

    task-2

--- script.js
+++ script.js
@@ -62,7 +62,7 @@ let email = "naveen9065@gmail.com";
 
 console.log("Name : " + name);
 console.log("Age : " + age);
-console.log("Phone : " + phone);
+console.log("Phone : " + phone);3
 console.log("Email : " + email);
 
 
@@ -71,4 +71,146 @@ let name1 = prompt("Enter your name:");
 let course1 = prompt("Enter your course:");
 
 alert("Hello " + name + ", Welcome to " + course + " Course");
-j
+
+
+
+let marks = 75;
+
+let result = (marks >= 35) ? "Pass" : "Fail";
+
+let grade = (marks >= 90) ? "Grade A" :
+            (marks >= 75) ? "Grade B" :
+            (marks >= 60) ? "Grade C" :
+            "No Grade";
+
+console.log("Marks:", marks);
+console.log("Result:", result);
+console.log("Grade:", grade);
+
+
+
+let salary = 25000;
+let bonus = salary * 0.10;
+let finalSalary = salary + bonus;
+
+console.log("Original Salary :", salary);
+console.log("Bonus :", bonus);
+console.log("Final Salary :", finalSalary);
+
+
+
+let email = "admin@gmail.com";
+let password = "12345";
+
+if (email === "admin@gmail.com" && password === "12345") {
+    console.log("Login Success");
+} else {
+    console.log("Login Failed");
+}
+
+
+
+let price = 2000;
+let discount = (price > 1000) ? price * 0.20 : 0;
+let finalAmount = price - discount;
+console.log("Original Price :", price);
+console.log("Discount :", discount);
+console.log("Final Amount :", finalAmount);
+
+
+
+let products = ["Laptop", "Mobile", "Mouse", "Keyboard"];
+console.log("First Product :", products[0]);
+console.log("Last Product :", products[products.length - 1]);
+console.log("Total Products :", products.length);
+
+
+
+let employee = {
+    name: "Naveen",
+    department: "Development",
+    salary: 30000
+};
+console.log(employee);
+console.log("Employee Name :", employee.name);
+console.log("Salary :", employee.salary);
+
+
+
+let shirt = 500;
+let pant = 1000;
+let shoe = 1500;
+let totalBill = shirt + pant + shoe;
+let discount1 = totalBill * 0.10;
+let finalAmount1 = totalBill - discount;
+console.log("Total Bill :", totalBill);
+console.log("Discount1 :", discount1);
+console.log("Final Amount1 :", finalAmount1);
+
+
+let age = 19;
+
+if (age >= 18) {
+    console.log("Eligible for Voting");
+} else {
+    console.log("Not Eligible");
+}
+
+
+
+let a = 10;
+
+console.log("Initial Value :", a);
+
+a++;
+console.log("After a++ :", a);
+
+++a;
+console.log("After ++a :", a);
+
+a--;
+console.log("After a-- :", a);
+
+--a;
+console.log("After --a :", a);
+
+
+
+let isPresent = true;
+let completedTask = true;
+
+
+if (isPresent && completedTask) {
+    console.log("Eligible for Salary");
+} else {
+    console.log("Not Eligible");
+}
+
+
+
+
+let employee1 = {
+    id: 101,
+    name: "John",
+    department: "Development",
+    salary: 35000,
+    experience: 2
+};
+
+
+console.log("Employee ID :", employee.id);
+console.log("Employee Name :", employee.name);
+console.log("Department :", employee.department);
+console.log("Salary :", employee.salary);
+console.log("Experience :", employee.experience, "Years");
+
+
+let bonus1 = employee.salary * 0.10;
+let finalSalary1 = employee.salary + bonus;
+
+console.log("\nBonus :", bonus);
+console.log("Final Salary :", finalSalary);
+
+let performanceStatus = (employee.experience >= 2) ? "Good" : "Average";
+
+console.log("\nPerformance Status :", performanceStatus);
