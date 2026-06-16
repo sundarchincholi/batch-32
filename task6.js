222499592abd3efc2280af1eb49931bdc92d615a
sundarchincholi <sundarchincholi75@gmail.com>
2026-06-16 10:48:16 +0530

    task-7 added

--- task6.js
+++ task6.js
@@ -1,367 +1,367 @@
 
-let employeeName = "Rahul";
-let salary = 30000;
-let experience = 4;
+// let employeeName = "Rahul";
+// let salary = 30000;
+// let experience = 4;
 
-let bonus = 0;
+// let bonus = 0;
 
-if (experience >= 5) {
-    bonus = salary * 0.20;
-} else if (experience >= 2) {
-    bonus = salary * 0.10;
-} else {
-    bonus = salary * 0.05;
-}
+// if (experience >= 5) {
+//     bonus = salary * 0.20;
+// } else if (experience >= 2) {
+//     bonus = salary * 0.10;
+// } else {
+//     bonus = salary * 0.05;
+// }
 
 
-let finalSalary = salary + bonus;
+// let finalSalary = salary + bonus;
 
-console.log("Employee Name :", employeeName);
-console.log("Salary :", salary);
-console.log("Experience :", experience, "Years");
-console.log("Bonus :", bonus);
-console.log("Final Salary :", finalSalary);
+// console.log("Employee Name :", employeeName);
+// console.log("Salary :", salary);
+// console.log("Experience :", experience, "Years");
+// console.log("Bonus :", bonus);
+// console.log("Final Salary :", finalSalary);
 
 
-let studentName = "Sundar";
-let age = 18;
-let percentage = 75;
+// let studentName = "Sundar";
+// let age = 18;
+// let percentage = 75;
 
 
-if (age >= 17) {
-    if (percentage >= 60) {
-        console.log("Student Name :", studentName);
-        console.log("Admission Approved");
-    } else {
-        console.log("Student Name :", studentName);
-        console.log("Admission Rejected");
-    }
-} else {
-    console.log("Student Name :", studentName);
-    console.log("Admission Rejected");
-}
+// if (age >= 17) {
+//     if (percentage >= 60) {
+//         console.log("Student Name :", studentName);
+//         console.log("Admission Approved");
+//     } else {
+//         console.log("Student Name :", studentName);
+//         console.log("Admission Rejected");
+//     }
+// } else {
+//     console.log("Student Name :", studentName);
+//     console.log("Admission Rejected");
+// }
 
 
 
-let choice = 1;
+// let choice = 1;
 
-switch (choice) {
-    case 1:
-        console.log("Order Confirmed : Pizza");
-        break;
+// switch (choice) {
+//     case 1:
+//         console.log("Order Confirmed : Pizza");
+//         break;
 
-    case 2:
-        console.log("Order Confirmed : Burger");
-        break;
+//     case 2:
+//         console.log("Order Confirmed : Burger");
+//         break;
 
-    case 3:
-        console.log("Order Confirmed : Shawarma");
-        break;
+//     case 3:
+//         console.log("Order Confirmed : Shawarma");
+//         break;
 
-    case 4:
-        console.log("Order Confirmed : Fried Rice");
-        break;
+//     case 4:
+//         console.log("Order Confirmed : Fried Rice");
+//         break;
 
-    default:
-        console.log("Invalid Selection");
-}
+//     default:
+//         console.log("Invalid Selection");
+// }
 
 
 
-let students = [
-    "Rahul",
-    "Kiran",
-    "Sundar",
-    "Naveen",
-    "Arjun",
-    "Ravi",
-    "Manoj",
-    "Deepak",
-    "Vikas",
-    "Ajay"
-];
+// let students = [
+//     "Rahul",
+//     "Kiran",
+//     "Sundar",
+//     "Naveen",
+//     "Arjun",
+//     "Ravi",
+//     "Manoj",
+//     "Deepak",
+//     "Vikas",
+//     "Ajay"
+// ];
 
 
-console.log("Student List:");
-for (let student of students) {
-    console.log(student);
-}
+// console.log("Student List:");
+// for (let student of students) {
+//     console.log(student);
+// }
 
-console.log("Total Students :", students.length);
+// console.log("Total Students :", students.length);
 
 
-let cart = [
-    { product: "Mobile", price: 15000 },
-    { product: "Headset", price: 2000 },
-    { product: "Charger", price: 1000 }
-];
+// let cart = [
+//     { product: "Mobile", price: 15000 },
+//     { product: "Headset", price: 2000 },
+//     { product: "Charger", price: 1000 }
+// ];
 
 
-console.log("Products in Cart:");
-for (let item of cart) {
-    console.log(item.product);
-}
+// console.log("Products in Cart:");
+// for (let item of cart) {
+//     console.log(item.product);
+// }
 
 
-let total = 0;
+// let total = 0;
 
-for (let item of cart) {
-    total += item.price;
-}
+// for (let item of cart) {
+//     total += item.price;
+// }
 
-console.log("Total Cart Value :", total);
+// console.log("Total Cart Value :", total);
 
-let expensiveProduct = cart[0];
+// let expensiveProduct = cart[0];
 
-for (let item of cart) {
-    if (item.price > expensiveProduct.price) {
-        expensiveProduct = item;
-    }
-}
+// for (let item of cart) {
+//     if (item.price > expensiveProduct.price) {
+//         expensiveProduct = item;
+//     }
+// }
 
-console.log("Most Expensive Product :", expensiveProduct.product);
-console.log("Price :", expensiveProduct.price);
+// console.log("Most Expensive Product :", expensiveProduct.product);
+// console.log("Price :", expensiveProduct.price);
 
 
-let balance = 10000;
+// let balance = 10000;
 
 
-function deposit(amount) {
-    balance += amount;
-    console.log("Deposited :", amount);
-}
+// function deposit(amount) {
+//     balance += amount;
+//     console.log("Deposited :", amount);
+// }
 
 
-function withdraw(amount) {
-    if (amount <= balance) {
-        balance -= amount;
-        console.log("Withdrawn :", amount);
-    } else {
-        console.log("Insufficient Balance");
-    }
-}
+// function withdraw(amount) {
+//     if (amount <= balance) {
+//         balance -= amount;
+//         console.log("Withdrawn :", amount);
+//     } else {
+//         console.log("Insufficient Balance");
+//     }
+// }
 
 
-function checkBalance() {
-    console.log("Current Balance :", balance);
-}
+// function checkBalance() {
+//     console.log("Current Balance :", balance);
+// }
 
 
-checkBalance();
+// checkBalance();
 
-deposit(5000);
-checkBalance();
+// deposit(5000);
+// checkBalance();
 
-withdraw(3000);
-checkBalance();
+// withdraw(3000);
+// checkBalance();
 
 
 
 
-let age1 = 25;
+// let age1 = 25;
 
 
-if (age < 5) {
-    console.log("Ticket Price : Free Ticket");
-}
-else if (age >= 5 && age < 18) {
-    console.log("Ticket Price : ₹100");
-}
-else if (age >= 18 && age <= 60) {
-    console.log("Ticket Price : ₹200");
-}
-else {
-    console.log("Ticket Price : ₹120");
-}
+// if (age < 5) {
+//     console.log("Ticket Price : Free Ticket");
+// }
+// else if (age >= 5 && age < 18) {
+//     console.log("Ticket Price : ₹100");
+// }
+// else if (age >= 18 && age <= 60) {
+//     console.log("Ticket Price : ₹200");
+// }
+// else {
+//     console.log("Ticket Price : ₹120");
+// }
 
 
 
 
 
-let purchaseAmount = 6000;
+// let purchaseAmount = 6000;
 
-let discount = 0;
-let finalAmount = 0;
+// let discount = 0;
+// let finalAmount = 0;
 
-if (purchaseAmount > 5000) {
-    discount = purchaseAmount * 0.20;
-}
-else if (purchaseAmount > 3000) {
-    discount = purchaseAmount * 0.10;
-}
-else if (purchaseAmount > 1000) {
-    discount = purchaseAmount * 0.05;
-}
-else {
-    discount = 0;
-}
+// if (purchaseAmount > 5000) {
+//     discount = purchaseAmount * 0.20;
+// }
+// else if (purchaseAmount > 3000) {
+//     discount = purchaseAmount * 0.10;
+// }
+// else if (purchaseAmount > 1000) {
+//     discount = purchaseAmount * 0.05;
+// }
+// else {
+//     discount = 0;
+// }
 
-finalAmount = purchaseAmount - discount;
+// finalAmount = purchaseAmount - discount;
 
-console.log("Original Amount :", purchaseAmount);
-console.log("Discount :", discount);
-console.log("Final Amount :", finalAmount);
+// console.log("Original Amount :", purchaseAmount);
+// console.log("Discount :", discount);
+// console.log("Final Amount :", finalAmount);
 
 
-let inventory = ["Rice", "Oil", "Sugar", "Milk", "Egg"];
+// let inventory = ["Rice", "Oil", "Sugar", "Milk", "Egg"];
 
-console.log("Original Inventory:");
-console.log(inventory);
+// console.log("Original Inventory:");
+// console.log(inventory);
 
 
-inventory.push("Bread");
-inventory.push("Butter");
+// inventory.push("Bread");
+// inventory.push("Butter");
 
 
-inventory.shift();
+// inventory.shift();
 
 
-inventory.pop();
+// inventory.pop();
 
 
-if (inventory.includes("Milk")) {
-    console.log("Milk exists in inventory");
-} else {
-    console.log("Milk does not exist in inventory");
-}
+// if (inventory.includes("Milk")) {
+//     console.log("Milk exists in inventory");
+// } else {
+//     console.log("Milk does not exist in inventory");
+// }
 
-console.log("Final Inventory:");
-console.log(inventory);
+// console.log("Final Inventory:");
+// console.log(inventory);
 
-let patient = {
-    patientName: "Ravi Kumar",
-    age: 45,
-    disease: "Fever",
-    doctor: "Dr. Sharma"
-};
+// let patient = {
+//     patientName: "Ravi Kumar",
+//     age: 45,
+//     disease: "Fever",
+//     doctor: "Dr. Sharma"
+// };
 
 
-console.log("Patient Details:");
+// console.log("Patient Details:");
 
-for (let key in patient) {
-    console.log(key + " : " + patient[key]);
-}
+// for (let key in patient) {
+//     console.log(key + " : " + patient[key]);
+// }
 
-let { patientName, age2, disease, doctor } = patient;
+// let { patientName, age2, disease, doctor } = patient;
 
-console.log("\nExtracted Values:");
-console.log("Patient Name :", patientName);
-console.log("Age :", age);
-console.log("Disease :", disease);
-console.log("Doctor :", doctor);
+// console.log("\nExtracted Values:");
+// console.log("Patient Name :", patientName);
+// console.log("Age :", age);
+// console.log("Disease :", disease);
+// console.log("Doctor :", doctor);
 
 
 
-function sendSMS() {
-    console.log("SMS Sent To Customer");
-}
+// function sendSMS() {
+//     console.log("SMS Sent To Customer");
+// }
 
-function placeOrder(callback) {
-    console.log("Order Placed Successfully");
-    callback();
-}
+// function placeOrder(callback) {
+//     console.log("Order Placed Successfully");
+//     callback();
+// }
 
-placeOrder(sendSMS);
+// placeOrder(sendSMS);
 
 
 
 
 
 
-function* cashbackOffers() {
-    yield "10% Cashback";
-    yield "20% Cashback";
-    yield "Free Delivery";
-    yield "Buy 1 Get 1";
-}
-let offers = cashbackOffers();
-console.log(offers.next().value);
-console.log(offers.next().value);
-console.log(offers.next().value);
-console.log(offers.next().value);
+// function* cashbackOffers() {
+//     yield "10% Cashback";
+//     yield "20% Cashback";
+//     yield "Free Delivery";
+//     yield "Buy 1 Get 1";
+// }
+// let offers = cashbackOffers();
+// console.log(offers.next().value);
+// console.log(offers.next().value);
+// console.log(offers.next().value);
+// console.log(offers.next().value);
 
 
 
 
-let employees = [
-    { id: 1, name: "Rahul", salary: 25000 },
-    { id: 2, name: "Kavin", salary: 30000 },
-    { id: 3, name: "John", salary: 40000 }
-];
+// let employees = [
+//     { id: 1, name: "Rahul", salary: 25000 },
+//     { id: 2, name: "Kavin", salary: 30000 },
+//     { id: 3, name: "John", salary: 40000 }
+// ];
 
 
-console.log("Employee Names:");
-for (let employee of employees) {
-    console.log(employee.name);
-}
+// console.log("Employee Names:");
+// for (let employee of employees) {
+//     console.log(employee.name);
+// }
 
 
-let totalSalary = 0;
+// let totalSalary = 0;
 
-for (let employee of employees) {
-    totalSalary += employee.salary;
-}
+// for (let employee of employees) {
+//     totalSalary += employee.salary;
+// }
 
-console.log("Total Salary Expense :", totalSalary);
+// console.log("Total Salary Expense :", totalSalary);
 
-let highestSalaryEmployee = employees[0];
+// let highestSalaryEmployee = employees[0];
 
-for (let employee of employees) {
-    if (employee.salary > highestSalaryEmployee.salary) {
-        highestSalaryEmployee = employee;
-    }
-}
+// for (let employee of employees) {
+//     if (employee.salary > highestSalaryEmployee.salary) {
+//         highestSalaryEmployee = employee;
+//     }
+// }
 
-console.log("Highest Salary Employee :", highestSalaryEmployee.name);
-console.log("Salary :", highestSalaryEmployee.salary);
+// console.log("Highest Salary Employee :", highestSalaryEmployee.name);
+// console.log("Salary :", highestSalaryEmployee.salary);
 
 
 
 
-let availableSeats = 50;
+// let availableSeats = 50;
 
-function bookSeats(seatsRequired) {
-    if (seatsRequired <= availableSeats) {
-        availableSeats -= seatsRequired;
+// function bookSeats(seatsRequired) {
+//     if (seatsRequired <= availableSeats) {
+//         availableSeats -= seatsRequired;
 
-        console.log("Booking Successful");
-        console.log("Seats Booked :", seatsRequired);
-        console.log("Available Seats :", availableSeats);
-    } else {
-        console.log("Booking Rejected");
-        console.log("Seats Unavailable");
-    }
-}
-bookSeats(10);
-bookSeats(15);
-bookSeats(30);
+//         console.log("Booking Successful");
+//         console.log("Seats Booked :", seatsRequired);
+//         console.log("Available Seats :", availableSeats);
+//     } else {
+//         console.log("Booking Rejected");
+//         console.log("Seats Unavailable");
+//     }
+// }
+// bookSeats(10);
+// bookSeats(15);
+// bookSeats(30);
 
 
 
-let products = {
-    Mobile: 15000,
-    Laptop: 50000,
-    Headphone: 2000,
-    Smartwatch: 3000
-};
+// let products = {
+//     Mobile: 15000,
+//     Laptop: 50000,
+//     Headphone: 2000,
+//     Smartwatch: 3000
+// };
 
 
-let selectedProducts = ["Mobile", "Headphone", "Smartwatch"];
+// let selectedProducts = ["Mobile", "Headphone", "Smartwatch"];
 
-let totalAmount = 0;
+// let totalAmount = 0;
 
-console.log("Selected Products:");
+// console.log("Selected Products:");
 
-for (let product of selectedProducts) {
-    console.log(product);
-    totalAmount += products[product];
-}
+// for (let product of selectedProducts) {
+//     console.log(product);
+//     totalAmount += products[product];
+// }
 
-let gst = totalAmount * 0.18;
+// let gst = totalAmount * 0.18;
 
 
-let finalBill = totalAmount + gst;
+// let finalBill = totalAmount + gst;
 
-console.log("Total Amount : ₹" + totalAmount);
-console.log("GST (18%) : ₹" + gst);
-console.log("Final Bill : ₹" + finalBill);
+// console.log("Total Amount : ₹" + totalAmount);
+// console.log("GST (18%) : ₹" + gst);
+// console.log("Final Bill : ₹" + finalBill);
---
+++ task7.js
@@ -0,0 +1,209 @@
+// Task 1: Traffic Signal Management
+
+let signalColor = prompt("Enter Signal Color (Red, Yellow, Green):");
+
+switch (signalColor.toLowerCase()) {
+    case "red":
+        console.log("Action: Stop");
+        break;
+
+    case "yellow":
+        console.log("Action: Get Ready");
+        break;
+
+    case "green":
+        console.log("Action: Go");
+        break;
+
+    default:
+        console.log("Action: Invalid Signal");
+}
+for(let i = 1; i <= 30; i++) {
+    console.log("Employee " + i + " Present");
+}
+let balance = 10000;
+
+while(balance > 0) {
+    balance -= 500;
+    console.log("Remaining Balance: ₹" + balance);
+}
+let age = 18;
+let percentage = 75;
+let entranceExamPassed = true;
+
+if(age >= 18) {
+    if(percentage >= 70) {
+        if(entranceExamPassed) {
+            console.log("Admission Approved");
+        } else {
+            console.log("Rejected: Entrance Exam Not Passed");
+        }
+    } else {
+        console.log("Rejected: Percentage Below 70");
+    }
+} else {
+    console.log("Rejected: Age Below 18");
+}
+function deliveryStatus(customerName, foodItem, deliveryAddress) {
+    console.log(
+        "Hello " + customerName +
+        ", your order of " + foodItem +
+        " will be delivered to " + deliveryAddress
+    );
+}
+
+deliveryStatus("Rahul", "Pizza", "Chennai");
+function calculateSalary() {
+    return 30000;
+}
+
+function calculateBonus(salary) {
+    return salary + (salary * 10 / 100);
+}
+
+let salary = calculateSalary();
+let finalSalary = calculateBonus(salary);
+
+console.log("Salary:", salary);
+console.log("Final Salary:", finalSalary);
+function applyDiscount(amount, callback) {
+    let discountedAmount = amount - (amount * 10 / 100);
+    callback(discountedAmount);
+}
+
+function generateInvoice(finalAmount) {
+    console.log("Invoice Generated");
+    console.log("Final Amount: ₹" + finalAmount);
+}
+
+applyDiscount(5000, generateInvoice);
+function* offers() {
+    yield "10% OFF";
+    yield "20% OFF";
+    yield "Free Delivery";
+    yield "Buy 1 Get 1";
+}
+
+let offer = offers();
+
+console.log(offer.next().value);
+console.log(offer.next().value);
+console.log(offer.next().value);
+console.log(offer.next().value);
+let scienceStudents = ["Rahul", "Kiran", "Ajay"];
+let commerceStudents = ["Priya", "Anu", "Ravi"];
+
+let allStudents = [...scienceStudents, ...commerceStudents];
+
+console.log(allStudents);
+function calculateMarks(studentName, ...marks) {
+    let total = 0;
+
+    for(let mark of marks) {
+        total += mark;
+    }
+
+    console.log("Student:", studentName);
+    console.log("Total Marks:", total);
+}
+
+calculateMarks("Rahul", 80, 90, 85, 95, 88);
+let employee = {
+    name: "Rahul",
+    department: "Development",
+    salary: 50000,
+    experience: 3
+};
+
+let {name, department, salary1, experience} = employee;
+
+console.log(name);
+console.log(department);
+console.log(salary);
+console.log(experience);
+let products = [
+    {name:"Laptop", price:50000, category:"Electronics"},
+    {name:"Mouse", price:800, category:"Electronics"},
+    {name:"Mobile", price:25000, category:"Electronics"},
+    {name:"Keyboard", price:1200, category:"Electronics"}
+];
+
+let premiumProducts = products.filter(product => product.price > 5000);
+
+console.log(premiumProducts);
+let customers = [
+    {name:"Rahul", purchase:30000},
+    {name:"Kiran", purchase:60000},
+    {name:"Ajay", purchase:70000}
+];
+
+let premiumCustomer = customers.find(
+    customer => customer.purchase > 50000
+);
+
+console.log(premiumCustomer);
+let expenses = [10000, 15000, 25000, 5000];
+
+let totalExpense = expenses.reduce(
+    (total, expense) => total + expense,
+    0
+);
+
+console.log("Total Expense: ₹" + totalExpense);
+let players = [20, 25, 22, 19, 30];
+
+let result = players.every(age => age > 18);
+
+console.log(result);
+let candidates = [
+    {name:"Rahul", skill:"Java"},
+    {name:"Kiran", skill:"React"},
+    {name:"Ajay", skill:"Python"}
+];
+
+let reactDeveloper = candidates.some(
+    candidate => candidate.skill === "React"
+);
+
+console.log(reactDeveloper);
+let mobile = "9876543210";
+
+if(
+    mobile.length === 10 &&
+    (mobile.startsWith("6") ||
+     mobile.startsWith("7") ||
+     mobile.startsWith("8") ||
+     mobile.startsWith("9"))
+) {
+    console.log("Valid Mobile Number");
+} else {
+    console.log("Invalid Mobile Number");
+}
+let title = "Learn JavaScript Complete Course";
+
+let slug = title.toLowerCase().split(" ").join("-");
+
+console.log(slug);
+let employees = [
+    {name:"Rahul", salary:30000},
+    {name:"Kiran", salary:50000},
+    {name:"Ajay", salary:40000}
+];
+
+// Highest to Lowest
+employees.sort((a, b) => b.salary - a.salary);
+console.log(employees);
+
+// Lowest to Highest
+employees.sort((a, b) => a.salary - b.salary);
+console.log(employees);
+let movies = ["Leo", "Jailer", "Vikram", "Master"];
+
+let bookingIds = movies.map(
+    (movie, index) => ({
+        movieName: movie,
+        bookingId: "MOV" + (index + 1)
+    })
+);
+
+console.log(bookingIds);
